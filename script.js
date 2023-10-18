c9ed7debaf87b1a01d2c2bd4e371f39a14493685
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-18 10:09:24 +0200

    innerHTML done

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("probando")
+
+//innerText vs innerHTML
+let containerNode = document.querySelector("#container")
+
+console.log(containerNode.innerText) // contenido texto del nodo
+
+console.log(containerNode.innerHTML) // un string que representa todas los elementos de DOM dentro de este nodo
+
+// containerNode.innerText = "<h1>patata</h1>"
+
+// containerNode.innerHTML = "<h2>Titulo agregado <span>con un span adentro</span> </h2>"
+
+let text = "con un span adentro"
+
+
+
+containerNode.innerHTML = `
+  <h2 id="title">Titulo agregado 
+    <span>${text}</span> 
+  </h2>
+`
+
